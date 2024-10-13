651634aa1e6cad9fca4c89e946512a9200fd529c
Unshack <marckfriencecoto30@gmail.com>
2024-10-13 15:32:55 +0800

    create component wix api display product from db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,8 @@ import { delay } from "@/lib/utils";
 import { getWixClient } from "@/lib/wix-client.base";
 import Product from "@/components/Products";
 import { Skeleton } from "@/components/ui/skeleton";
+import { getCollectionBySlug } from "@/wix-api/collections";
+import { queryProducts } from "@/wix-api/products";
 
 export default function Home() {
   return (
@@ -46,20 +48,15 @@ export default function Home() {
 async function FeaturedProducts() {
   await delay(1000);
 
-  const wixClient = getWixClient();
-
-  const { collection } =
-    await wixClient.collections.getCollectionBySlug("featured-products");
+  const collection = await getCollectionBySlug("featured-products");
 
   if (!collection?._id) {
     return null;
   }
 
-  const featuredProducts = await wixClient.products
-    .queryProducts()
-    .hasSome("collectionIds", [collection._id])
-    .descending("lastUpdated")
-    .find();
+  const featuredProducts = await queryProducts({
+    collectionIds: collection._id,
+  });
 
   if (!featuredProducts.items.length) {
     return null;
