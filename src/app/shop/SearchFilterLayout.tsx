11ac1703e2b5bcc5002cebec4b4283cdcdf479cc
Unshack <marckfriencecoto30@gmail.com>
2024-10-17 22:52:38 +0800

    checkbox filter render faster using useOptimistic hook

--- src/app/shop/SearchFilterLayout.tsx
+++ src/app/shop/SearchFilterLayout.tsx
@@ -3,6 +3,7 @@
 import { Checkbox } from "@/components/ui/checkbox";
 import { collections } from "@wix/stores";
 import { useRouter, useSearchParams } from "next/navigation";
+import { useOptimistic, useTransition } from "react";
 
 interface SearchFilterLayoutProps {
   collections: collections.Collection[];
@@ -16,7 +17,11 @@ export default function SearchFilterLayout({
   const router = useRouter();
   const searchParams = useSearchParams();
 
-  const selectedCollectionsIds = searchParams.getAll("collection");
+  const [optimisticCollectionIds, setOptimisticCollectionIds] = useOptimistic(
+    searchParams.getAll("collection"),
+  );
+
+  const [isPending, startTransition] = useTransition();
 
   function updateFilters(collectionIds: string[]) {
     const newSearchParams = new URLSearchParams(searchParams);
@@ -26,48 +31,71 @@ export default function SearchFilterLayout({
       newSearchParams.append("collection", collectionId);
     });
 
-    router.push(`?${newSearchParams.toString()}`);
+    startTransition(() => {
+      setOptimisticCollectionIds(collectionIds);
+      router.push(`?${newSearchParams.toString()}`);
+    });
   }
 
   return (
     <main className="flex flex-col items-center justify-center gap-10 px-5 py-10 lg:flex-row lg:items-start">
       <aside className="h-fit space-y-5 lg:sticky lg:top-10 lg:w-64">
-        <div className="space-y-3">
-          <div className="font-bold">Collections</div>
-          <ul className="space-y-1.5">
-            {collections.map((collection) => {
-              const collectionId = collection._id;
-              if (!collectionId) return null;
-              return (
-                <li key={collectionId}>
-                  <label className="flex cursor-pointer items-center gap-2 font-medium">
-                    <Checkbox
-                      id={collectionId}
-                      checked={selectedCollectionsIds.includes(collectionId)}
-                      onCheckedChange={(checked) => {
-                        updateFilters(
-                          checked
-                            ? [...selectedCollectionsIds, collectionId]
-                            : selectedCollectionsIds.filter(
-                                (id) => id !== collectionId,
-                              ),
-                        );
-                      }}
-                    />
-                    <span className="line-clamp-1 break-all">
-                      {collection.name}
-                    </span>
-                  </label>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+        <CollectionsFilter
+          collections={collections}
+          selectedCollectionIds={optimisticCollectionIds}
+          updateCollectionIds={updateFilters}
+        />
       </aside>
       <div className="w-full max-w-7xl space-y-5">
         <div className="flex justify-center lg:justify-end">sort filter:</div>
         {children}
       </div>
     </main>
+  );
+}
+
+interface CollectionsFilterProps {
+  collections: collections.Collection[];
+  selectedCollectionIds: string[];
+  updateCollectionIds: (collectionIds: string[]) => void;
+}
+
+function CollectionsFilter({
+  collections,
+  selectedCollectionIds,
+  updateCollectionIds,
+}: CollectionsFilterProps) {
+  return (
+    <div className="space-y-3">
+      <div className="font-bold">Collections</div>
+      <ul className="space-y-1.5">
+        {collections.map((collection) => {
+          const collectionId = collection._id;
+          if (!collectionId) return null;
+          return (
+            <li key={collectionId}>
+              <label className="flex cursor-pointer items-center gap-2 font-medium">
+                <Checkbox
+                  id={collectionId}
+                  checked={selectedCollectionIds.includes(collectionId)}
+                  onCheckedChange={(checked) => {
+                    updateCollectionIds(
+                      checked
+                        ? [...selectedCollectionIds, collectionId]
+                        : selectedCollectionIds.filter(
+                            (id) => id !== collectionId,
+                          ),
+                    );
+                  }}
+                />
+                <span className="line-clamp-1 break-all">
+                  {collection.name}
+                </span>
+              </label>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 }
