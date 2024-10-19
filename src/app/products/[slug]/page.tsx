edddfcd3b67b92b0ad556f36827f60c69ffebf44
Unshack <marckfriencecoto30@gmail.com>
2024-10-19 21:40:17 +0800

    related products functionaluty

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,9 +1,12 @@
-import { getProductBySlug } from "@/wix-api/products";
+import { getProductBySlug, getRelatedProducts } from "@/wix-api/products";
 import { notFound } from "next/navigation";
 import ProductDetails from "./ProductDetails";
 import { Metadata } from "next";
 import { delay } from "@/lib/utils";
 import { getWixServerClient } from "@/lib/wix-client.server";
+import Product from "@/components/Products";
+import { Suspense } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface PageProps {
   params: { slug: string };
@@ -46,6 +49,51 @@ export default async function Page({ params: { slug } }: PageProps) {
   return (
     <main className="mx-auto max-w-7xl space-y-10 px-5 py-10">
       <ProductDetails product={product} />
+      <hr />
+      <Suspense fallback={<RelatedProductsLoadingSkeleton />}>
+        <RelatedProducts productId={product._id} />
+      </Suspense>
+      <hr />
+      <div className="space-y-5">
+        <h2 className="text-2xl font-bold">Buyer reviews</h2>
+        {/* <Suspense fallback={<ProductReviewsLoadingSkeleton />}>
+          <ProductReviewsSection product={product} />
+        </Suspense> */}
+      </div>
     </main>
+  );
+}
+
+interface RelatedProductsProps {
+  productId: string;
+}
+
+async function RelatedProducts({ productId }: RelatedProductsProps) {
+  const relatedProducts = await getRelatedProducts(
+    getWixServerClient(),
+    productId,
+  );
+
+  if (!relatedProducts.length) return null;
+
+  return (
+    <div className="space-y-5">
+      <h2 className="text-2xl font-bold">Related Products</h2>
+      <div className="flex grid-cols-2 flex-col gap-5 sm:grid lg:grid-cols-4">
+        {relatedProducts.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function RelatedProductsLoadingSkeleton() {
+  return (
+    <div className="flex grid-cols-2 flex-col gap-5 pt-12 sm:grid lg:grid-cols-4">
+      {Array.from({ length: 4 }).map((_, i) => (
+        <Skeleton key={i} className="h-[26rem] w-full" />
+      ))}
+    </div>
   );
 }
